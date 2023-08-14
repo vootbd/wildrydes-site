52948fa7e8aa1751fcd885cb2497b08ad5b87b79
john <johnmaxwellgomes@gmail.com>
2023-08-14 13:56:30 +0600

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_Az3BMSF0A', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7hds894p48kg7rvuhhumqbia4n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
